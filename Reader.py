84202de6418f66f3e105373521d88a3442e176d6
Mikey Christoferis Qu4rkz <71943288+Christoferis@users.noreply.github.com>
2021-03-27 14:05:20 +0100

    Whitelister implemented

--- Reader.py
+++ Reader.py
@@ -6,59 +6,71 @@
 #Duplicates (Server) and Name herausfischen
 #whitelist syntax
 #second script for looking up the MC Username and infos
+#add in 8 Player barrier
 
 
 #Imports
 import csv
+from typing import Type
 import mojang as mj
 import json
 
 
 #Variables
+max_players = 8
 
 csvpath = "The Factory All-Stars (Minecraft Event).csv"
-whitelist = "whitelist.json"
+whitelist_path = "whitelist.json"
 participants = dict()
+
 
 #format mc uuid to the 8-4-4-4-12 format
 def uuid_formatter(uuid):
-    listuuid = list(uuid)
-    
-    #add in the dashes
-    listuuid.insert(8, "-")
-    listuuid.insert(8+4+1, "-")
-    listuuid.insert(8+4+4+2, "-")
-    listuuid.insert(8+4+4+4+3, "-")
-    
+    #failsafe pt 1 if no player with that name
+    try:
+        listuuid = list(uuid)
+        
+        #add in the dashes
+        listuuid.insert(8, "-")
+        listuuid.insert(8+4+1, "-")
+        listuuid.insert(8+4+4+2, "-")
+        listuuid.insert(8+4+4+4+3, "-")
+        
 
-    final = str(listuuid).replace("[", '').replace("]", '').replace(",", '').replace("'", '').replace(" ", '')
+        final = str(listuuid).replace("[", '').replace("]", '').replace(",", '').replace("'", '').replace(" ", '')
+    except TypeError:
+        final = None
 
     return final
 
 
 def whitelist():
     global participants
-    global whitelist
+    global whitelist_path
+
     #open whitelist
-    openobj = open(whitelist, mode='w').write()
+    openobj = open(whitelist_path, mode='w')
     saveobj = list()
 
     #get minecraft names and uuids
 
     for player in participants:
+        #get the user entry
+        data = participants[player]
+
         #create whitelist container for every person
         container = {
-            "uuid": uuid_formatter(mj.MojangAPI.get_uuid(participants.get(player)[0])),
-            "name": uuid_formatter(participants.get(player)[0])
+            "uuid": uuid_formatter(mj.MojangAPI.get_uuid(data[0])),
+            "name": data[0],
         }
 
-        saveobj.append(container)
+        #failsafe pt 2
+        if container["uuid"] != None:
+            saveobj.append(container)
 
-    print(saveobj)
         
-
     #dump json in file
-    json.dump()
+    json.dump(fp=openobj, obj=saveobj)
     openobj.close()
     pass
 
@@ -70,6 +82,7 @@ def save_json():
 def csv_to_dict():
     global csvpath
     global participants
+    global max_players
 
     openobj = open(csvpath, newline='', mode='r')
 
@@ -83,18 +96,21 @@ def csv_to_dict():
         if iter != 0:
             participants[row[1]] = [row[2], row[3]]
         #only 8 participants
-        elif iter == 8:
+        elif iter == max_players:
             break
         else:
             iter += 1 
             pass
 
-        
 
 def main():
     print("reading csv")
+
     #get
     csv_to_dict()
+
+    #construct whitelist
+    whitelist()
 
 
 main()
