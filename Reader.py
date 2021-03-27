bca424f5cfa011e82062822e070f48e01aeed68f
Mikey Christoferis Qu4rkz <71943288+Christoferis@users.noreply.github.com>
2021-03-27 13:48:03 +0100

    first interation of whitelist system

--- Reader.py
+++ Reader.py
@@ -10,7 +10,7 @@
 
 #Imports
 import csv
-import mojang
+import mojang as mj
 import json
 
 
@@ -20,16 +20,48 @@ csvpath = "The Factory All-Stars (Minecraft Event).csv"
 whitelist = "whitelist.json"
 participants = dict()
 
+#format mc uuid to the 8-4-4-4-12 format
+def uuid_formatter(uuid):
+    listuuid = list(uuid)
+    
+    #add in the dashes
+    listuuid.insert(8, "-")
+    listuuid.insert(8+4+1, "-")
+    listuuid.insert(8+4+4+2, "-")
+    listuuid.insert(8+4+4+4+3, "-")
+    
+
+    final = str(listuuid).replace("[", '').replace("]", '').replace(",", '').replace("'", '').replace(" ", '')
+
+    return final
+
+
 def whitelist():
     global participants
     global whitelist
     #open whitelist
-    openobj = open(whitelist, mode='w').read()
-    jsonobj = json.load(fp=openobj)
+    openobj = open(whitelist, mode='w').write()
+    saveobj = list()
 
+    #get minecraft names and uuids
 
+    for player in participants:
+        #create whitelist container for every person
+        container = {
+            "uuid": uuid_formatter(mj.MojangAPI.get_uuid(participants.get(player)[0])),
+            "name": uuid_formatter(participants.get(player)[0])
+        }
+
+        saveobj.append(container)
+
+    print(saveobj)
+        
+
+    #dump json in file
+    json.dump()
     openobj.close()
     pass
+
 
 def save_json():
     pass
@@ -50,11 +82,12 @@ def csv_to_dict():
         #dont include first row (questions)
         if iter != 0:
             participants[row[1]] = [row[2], row[3]]
+        #only 8 participants
+        elif iter == 8:
+            break
         else:
             iter += 1 
             pass
-    
-    print(participants)
 
         
 
@@ -65,3 +98,5 @@ def main():
 
 
 main()
+
+
